059c4995b4428022832915d6860be49e83a4e160
kuman514 <hadjadj0@gmail.com>
2025-06-11 03:41:31 +0900

    draft: make text-input avaliable of multi-line

--- src/shared/text-input/index.tsx
+++ src/shared/text-input/index.tsx
@@ -1,12 +1,21 @@
 interface Props {
+  isMultiLine: boolean;
   textValue: string;
   onChange: (newValue: string) => void;
 }
 
-export default function TextInput({ textValue, onChange }: Props) {
-  return (
+export default function TextInput({ isMultiLine, textValue, onChange }: Props) {
+  return isMultiLine ? (
+    <textarea
+      className="w-full px-6 py-4 border border-solid border-green-500 rounded-4xl"
+      value={textValue}
+      onChange={(event) => {
+        onChange(event.currentTarget.value);
+      }}
+    />
+  ) : (
     <input
-      className="px-6 py-4 border border-solid border-green-500 rounded-full"
+      className="w-full px-6 py-4 border border-solid border-green-500 rounded-full"
       value={textValue}
       onChange={(event) => {
         onChange(event.currentTarget.value);
