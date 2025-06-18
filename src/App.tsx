338cf01e40a59b6db5a6034fb08260ad5d9de574
kuman514 <hadjadj0@gmail.com>
2025-06-18 23:58:19 +0900

    draft: add appearances of post-article

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from 'react-router';
 
 import HomePage from '^/pages/home';
+import PostArticlePage from '^/pages/post-article';
 import PostTablePage from '^/pages/post-table';
 import RootLayout from '^/pages/root';
 
@@ -13,6 +14,7 @@ export default function App() {
 
           <Route path="/posts">
             <Route index element={<PostTablePage />} />
+            <Route path=":postId" element={<PostArticlePage />} />
           </Route>
         </Route>
       </Routes>
---
+++ src/pages/post-article/index.tsx
@@ -0,0 +1,27 @@
+export default function PostArticlePage() {
+  /**
+   * @todo
+   * Get post article using useQuery
+   * Get comments using useInfiniteQuery
+   */
+
+  return (
+    <section className="w-full max-w-4xl flex flex-col items-start text-center gap-8 p-24">
+      <div className="w-full flex flex-col items-start gap-4">
+        <span className="w-full text-left">게시글 ID</span>
+        <h1 className="w-full font-bold text-4xl border-b px-4 pb-4 text-left">
+          게시글 콘텐츠
+        </h1>
+      </div>
+      <ul className="w-full flex flex-col items-start [&_li]:flex [&_li]:flex-col [&_li]:items-start [&_li]:w-full [&_li]:even:bg-gray-200 [&_li]:p-2">
+        <li>댓글 1</li>
+        <li>댓글 2</li>
+        <li>댓글 3</li>
+        <li>댓글 4</li>
+        <li>댓글 5</li>
+        <li>댓글 6</li>
+        <li>댓글 7</li>
+      </ul>
+    </section>
+  );
+}
