ea6e321cfd99846180e2b2d325c0c3b8cfa5ced7
kuman514 <hadjadj0@gmail.com>
2025-06-22 11:30:16 +0900

    feat: add showing comments on a post

--- src/pages/post-article/index.tsx
+++ src/pages/post-article/index.tsx
@@ -1,8 +1,11 @@
-import { useQuery } from '@tanstack/react-query';
+import { useInfiniteQuery, useQuery } from '@tanstack/react-query';
 import { useParams } from 'react-router';
 
+import type { Comment } from '^/entities/comment/types';
 import type { Post } from '^/entities/post/types';
 import { apiClient } from '^/shared/api';
+import { ITEMS_PER_PAGE } from '^/shared/api/constants';
+import type { GetPagenatedApiResponse } from '^/shared/api/types';
 
 export default function PostArticlePage() {
   const { postId } = useParams();
@@ -12,16 +15,34 @@ export default function PostArticlePage() {
 
   /**
    * @todo
-   * Get comments using useInfiniteQuery
    * Add comments using useMutation
    */
 
-  const { data, isPending } = useQuery({
+  const { data: postData, isPending } = useQuery({
     queryKey: ['posts', postId],
     queryFn: async () => {
       const response = await apiClient.get<Post>(`/posts/${postId}`);
       return response.data;
     },
+  });
+
+  const {
+    data: commentData,
+    isFetching,
+    hasNextPage,
+    fetchNextPage,
+  } = useInfiniteQuery({
+    queryKey: ['comments', postData?.id],
+    queryFn: async ({ pageParam }) =>
+      (
+        await apiClient.get<GetPagenatedApiResponse<Comment>>(
+          `/comments?postId=${postData?.id}&_page=${pageParam}&_per_page=${ITEMS_PER_PAGE}`
+        )
+      ).data,
+    initialPageParam: 1,
+    getPreviousPageParam: (firstPage) => firstPage.prev,
+    getNextPageParam: (lastPage) => lastPage.next,
+    enabled: Boolean(postData?.id),
   });
 
   const renderPost = isPending ? (
@@ -30,25 +51,45 @@ export default function PostArticlePage() {
     </span>
   ) : (
     <>
-      <span className="w-full text-left">게시글 ID: {data?.id}</span>
+      <span className="w-full text-left">게시글 ID: {postData?.id}</span>
       <h1 className="w-full font-bold text-4xl border-b px-4 pb-4 text-left">
-        {data?.content}
+        {postData?.content}
       </h1>
     </>
   );
+
+  const renderComments = commentData?.pages
+    .reduce((prev, current) => prev.concat(current.data), Array<Comment>(0))
+    .map((comment) => <li key={comment.id}>{comment.content}</li>);
+
+  const renderSeeMoreCommentsButtonLabel = (() => {
+    if (isFetching) {
+      return '댓글 불러오는 중...';
+    }
+
+    if (!hasNextPage) {
+      return '댓글 모두 불러옴';
+    }
+
+    return '댓글 더 보기';
+  })();
 
   return (
     <section className="w-full max-w-4xl flex flex-col items-start text-center gap-8 p-24">
       <div className="w-full flex flex-col items-start gap-4">{renderPost}</div>
       <ul className="w-full flex flex-col items-start [&_li]:flex [&_li]:flex-col [&_li]:items-start [&_li]:w-full [&_li]:even:bg-gray-200 [&_li]:dark:even:bg-gray-700 [&_li]:p-2">
-        <li>댓글 1</li>
-        <li>댓글 2</li>
-        <li>댓글 3</li>
-        <li>댓글 4</li>
-        <li>댓글 5</li>
-        <li>댓글 6</li>
-        <li>댓글 7</li>
+        {renderComments}
       </ul>
+      <button
+        className="px-4 py-3 bg-green-500 text-white rounded-lg cursor-pointer hover:bg-green-300 disabled:bg-gray-300 disabled:cursor-auto"
+        disabled={isFetching || !hasNextPage}
+        onClick={() => {
+          fetchNextPage();
+        }}
+        type="button"
+      >
+        {renderSeeMoreCommentsButtonLabel}
+      </button>
     </section>
   );
 }
